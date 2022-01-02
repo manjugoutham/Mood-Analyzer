33b60fb4990a289073edf8f1805a5d64db4c96ef
manjugoutham <gouthamy219@gmail.com>
2022-01-02 13:36:21 +0530

    Mood analyzer problems

---
+++ Moodanalyzer/src/main/java/com/java/regex/Moodanalyzer/App.java
@@ -0,0 +1,13 @@
+package com.java.regex.Moodanalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Moodanalyzer/src/main/java/com/java/regex/Moodanalyzer/MoodAnalysisException.java
@@ -0,0 +1,12 @@
+package com.java.regex.Moodanalyzer;
+
+public class MoodAnalysisException extends Exception {
+	
+	String message;
+
+	public MoodAnalysisException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
